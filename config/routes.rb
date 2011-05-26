a080acfbddfd3af3322c78b7c9131f92e03e42d5
Mark Florian <mflorian2515@gmail.com>
2011-05-26 15:39:41 -0400

    Added a site layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApp::Application.routes.draw do
+
   get "pages/home"
-
   get "pages/contact"
+  get "pages/about"
 
-  # The priority is based upon order of creation:
+# The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
